c33316c915a4fce941ee9adf55930d05de3e5c29
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-05-06 18:35:31 -0700

    commit tree 9c6da7754d9c7c2620170c8f2ddec512c63517c6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "sadi",
+streetAddress: "1234 Main", 
+};
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+console.log(employee, key, value)
+let newEmployee= {...employee};
+newEmployee [key] = value;
+console.log(newEmployee)
+return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey (employee, key, value) {
+    let deleteEmployee= {...employee};
+    deleteEmployee [key] = value;
+    console.log(deleteEmployee)
+    return deleteEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
